64545e83fcfa914204c37fa528869881e3cd3f91
darya <daryashumeyko@gmail.com>
2019-12-18 18:56:59 +0300

    Полная реализация корзины

--- AdvertisingAgency/AdvertisingAgency/Controllers/CartController.cs
+++ AdvertisingAgency/AdvertisingAgency/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using AdvertisingAgency.Models.ViewModels.Cart;
+﻿using AdvertisingAgency.Models.Date;
+using AdvertisingAgency.Models.ViewModels.Cart;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,13 +52,16 @@ namespace AdvertisingAgency.Controllers
             if (Session["cart"] != null)
             {
                 //Получение общего количества товаров и цену
-                var list = (List<CartVM>)Session["cart"];
+                var list = (List<CartVM>) Session["cart"];
 
                 foreach (var item in list)
                 {
                     qty += item.Quantity;
                     price += item.Quantity * item.Price;
                 }
+
+                model.Quantity = qty;
+                model.Price = price;
             }
             else
             {
@@ -68,6 +72,127 @@ namespace AdvertisingAgency.Controllers
             
             //Возврат частичного представления с моделью
             return PartialView("_CartPartial", model);
+        }
+
+        public ActionResult AddToCartPartial(int id)
+        {
+            //Объявление List типа CartVM   (если сессия пуст асоздаётся новый лист)
+            List<CartVM> cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+
+            //Объявление модели CartVM
+            CartVM model = new CartVM();
+
+            using (Db db = new Db())
+            {
+                //Получение рекламы
+                ProductDTO product = db.Products.Find(id);
+
+                //Проверка нахождения рекламы в корзине
+                var productInCart = cart.FirstOrDefault(x => x.ProductId == id);
+
+                //Если нет, то добавление товара в корзину
+                if (productInCart == null)
+                {
+                    cart.Add(new CartVM()
+                    {
+                        ProductId = product.Id,
+                        ProductName = product.Name,
+                        Quantity = 1,
+                        Price = product.Price,
+                        Image = product.ImageName
+                    });
+                }
+
+                //Если есть, добавление единицы товара
+                else
+                {
+                    productInCart.Quantity++;
+                }
+            }
+
+            //Получение общего кол-ва рекламы, цену и добавление данных в модель
+            int qty = 0;
+            int price = 0;
+
+            foreach (var item in cart)
+            {
+                qty += item.Quantity;
+                price += item.Quantity * item.Price;
+            }
+
+            model.Quantity = qty;
+            model.Price = price;
+
+            //Созранение состояния корзины в сессию
+            Session["cart"] = cart;
+
+            //Возврат частичного представления с моделью
+            return PartialView("_AddToCartPartial", model);
+        }
+
+        //GET: /cart/IncrementProduct
+        public JsonResult IncrementProduct(int productId)
+        {
+            //Объявление list cart
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                //Получение модели CartVM из list
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                //Добавление количества
+                model.Quantity++;
+
+                //Сохранение необходимых данных
+                var result = new { qty = model.Quantity, price = model.Price };
+
+                //возврат Json ответ с данными
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //GET: /cart/DecrementProduct
+        public ActionResult DecrementProduct(int productId)
+        {
+            //Объявление list cart
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                //Получение модели CartVM из list
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                //Убавление количества
+                if (model.Quantity > 1)
+                    model.Quantity--;
+                else
+                {
+                    model.Quantity = 0;
+                    cart.Remove(model);
+                }
+
+                //Сохранение необходимых данных
+                var result = new { qty = model.Quantity, price = model.Price };
+
+                //возврат Json ответ с данными
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //GET: /cart/RemoveProduct
+        public void RemoveProduct(int productId)
+        {
+            //Объявление list cart
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                //Получение модели CartVM из list
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                cart.Remove(model);
+            }
         }
     }
 }
