1a7f47be17d3440f053e9e3eccfa42e5b032bbe5
darya <daryashumeyko@gmail.com>
2020-01-13 21:06:13 +0300

    т

---
+++ AdvertisingAgency/Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AdvertisingAgency.Models.ViewModels.Agency;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        
+        }
+    }
+}
